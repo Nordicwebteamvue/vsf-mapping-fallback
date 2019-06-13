08c1f1beda88b22e2c99098816eef81eaaa18eb9
Max Malm <benjick@dumfan.net>
2019-06-13 11:07:46 +0200

    forStoryblok: Fall back on home

--- builtin/forStoryblok.ts
+++ builtin/forStoryblok.ts
@@ -1,6 +1,7 @@
 import { Payload } from '../types/Payload'
 
 export const forStoryblok = async ({ dispatch }, { url: fullSlug }: Payload) => {
+  fullSlug = fullSlug || 'home'
   const story = await dispatch(`storyblok/loadStory`, { fullSlug }, { root: true })
   if (story) {
     return {
