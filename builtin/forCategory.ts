bad72e67e8a48a788648f55df3c01bd5f14ede6d
Anders Ekelund <anders.emil.ekelund@gmail.com>
2020-03-05 19:01:46 +0100

    Remove leading slash from url (1.11 backport)

--- builtin/forCategory.ts
+++ builtin/forCategory.ts
@@ -1,13 +1,14 @@
-import { removeStoreCodeFromRoute } from '@vue-storefront/core/lib/multistore'
+import { removeStoreCodeFromRoute, currentStoreView, localizedDispatcherRouteName } from '@vue-storefront/core/lib/multistore'
 import { Payload } from '../types/Payload'
 
 export const forCategory = async ({ dispatch }, { url }: Payload) => {
-  url = (removeStoreCodeFromRoute(url) as string)
+  const { storeCode, appendStoreCode } = currentStoreView()
+  url = (removeStoreCodeFromRoute(url.startsWith('/') ? url.slice(1) : url) as string)
   try {
     const category = await dispatch('category/single', { key: 'url_path', value: url }, { root: true })
     if (category !== null) {
       return {
-        name: 'category',
+        name: localizedDispatcherRouteName('category', storeCode, appendStoreCode),
         params: {
           slug: category.slug
         }
